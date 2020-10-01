8b33d58f288f784ca5260dabd81c7d5c7dbfdd62
SameeranP <51751179+SameeranP@users.noreply.github.com>
2020-10-01 13:38:25 +0530

    Update edge_algo.py

--- edge_algo.py
+++ edge_algo.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 28 13:48:48 2020
-
-@author: MCM
-"""
-
-
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -21,10 +13,13 @@ def edge_detection(mypath):
         edges = cv2.Canny(gblur,100,200)
         sobelx = cv2.Sobel(gblur,cv2.CV_64F,1,0,ksize=-1)  # x
         sobely = cv2.Sobel(gblur,cv2.CV_64F,0,1,ksize=-1)  # y
-
+        kernelx = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
+        kernely = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+        img_prewittx = cv2.filter2D(gblur, -1, kernelx)
+        img_prewitty = cv2.filter2D(gblur, -1, kernely)
         
                 
-        plt.subplot(1,4,1),plt.imshow(img,cmap = 'gray')
+        plt.subplot(1,6,1),plt.imshow(img,cmap = 'gray')
         plt.title('Original Image'), plt.xticks([]), plt.yticks([])
         #plt.subplot(1,4,2),plt.imshow(gray,cmap = 'gray')
         #plt.title('gray Image'), plt.xticks([]), plt.yticks([])
@@ -32,12 +27,17 @@ def edge_detection(mypath):
         #plt.title('mblur Image'), plt.xticks([]), plt.yticks([])
         #plt.subplot(4,1,4),plt.imshow(mblur,cmap = 'gray')
         #plt.title('gblur Image'), plt.xticks([]), plt.yticks([])
-        plt.subplot(1,4,2),plt.imshow(edges,cmap = 'gray')
+        plt.subplot(1,6,2),plt.imshow(edges,cmap = 'gray')
         plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-        plt.subplot(1,4,3),plt.imshow(sobelx,cmap = 'gray')
+        plt.subplot(1,6,3),plt.imshow(sobelx,cmap = 'gray')
         plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-        plt.subplot(1,4,4),plt.imshow(sobely,cmap = 'gray')
+        plt.subplot(1,6,4),plt.imshow(sobely,cmap = 'gray')
         plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+        plt.subplot(1,6,5),plt.imshow(sobely,cmap = 'gray')
+        plt.title('prewitt x'), plt.xticks([]), plt.yticks([])
+        plt.subplot(1,6,6),plt.imshow(sobely,cmap = 'gray')
+        plt.title('prewitt y'), plt.xticks([]), plt.yticks([])
+        
         
         plt.show()
 
